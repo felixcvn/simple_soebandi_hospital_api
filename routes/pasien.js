8ad860de33a8813deb7ea75b3ae51e941e360810
Lyxzz1 <egafelix1@gmail.com>
2025-07-17 20:05:17 +0700

    delete bcrypt and using bcryptjs instead

--- routes/pasien.js
+++ routes/pasien.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const Pasien = require('../models/pasien');
 const auth = require('../middleware/auth');
