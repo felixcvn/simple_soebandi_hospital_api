4289fbc0ac5f3acb6d1099589df690d9d4816f33
Lyxzz1 <egafelix1@gmail.com>
2025-07-17 20:33:05 +0700

    fixx

--- config/db.js
+++ config/db.js
@@ -1,4 +1,4 @@
-import { Sequelize } from 'sequelize';
+const { Sequelize } = require('sequelize');
 
 const sequelize = new Sequelize(
   process.env.DB_NAME,
@@ -11,4 +11,4 @@ const sequelize = new Sequelize(
   }
 );
 
-export default sequelize;
+module.exports = sequelize;
